f4097ef21591439cd130bcfa36513b54a775f519
Tanmay5454 <tiwaritanmay424@gmail.com>
2025-09-23 14:05:34 +0530

    added CORS middleware to app and updated requirements.txt to include cors package

--- app.py
+++ app.py
@@ -3,9 +3,21 @@ from typing import List
 import httpx
 import os
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 load_dotenv()
+
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 BASE_URL = os.getenv("BASE_URL")
 
